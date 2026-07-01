33a5e266f3546d588d25776177f310f281874a01
ART0067546 <ART0067546@SEN577988>
2026-07-01 13:48:20 -0300

    neo jump jump

--- Assets/Scripts/Inscripted.cs
+++ Assets/Scripts/Inscripted.cs
@@ -5,11 +5,45 @@ using UnityEngine;
 
 public class Inscripted : MonoBehaviour
 {
-    public float forca = 15f;
+    public float bounceForce = 25f;
+    public float squashAmount = 0.2f;
+    public float animationSpeed = 0.08f;
 
-    private void OnTriggerEnter(Collider other)
+    private Vector3 originalScale;
+
+    private void Start()
     {
-        Debug.Log("Colidiu com: " + other.name);
+        originalScale = transform.localScale;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+
+        if (rb != null)
+        {
+            Vector3 vel = rb.velocity;
+            vel.y = 0;
+            rb.velocity = vel;
+
+            rb.AddForce(Vector3.up * bounceForce, ForceMode.VelocityChange);
+
+            StartCoroutine(BounceAnimation());
+        }
+    }
+
+    IEnumerator BounceAnimation()
+    {
+        transform.localScale = new Vector3(
+            originalScale.x + squashAmount,
+            originalScale.y - squashAmount,
+            originalScale.z + squashAmount
+        );
+
+        yield return new WaitForSeconds(animationSpeed);
+
+        transform.localScale = originalScale;
     }
 }
+
 
